3d9ca393e802ee2227b74d5d6f6336ce29766861
Michael Bartlett <Michael.Bartlett@telus.com>
2024-02-06 22:14:52 -0800

    Commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -46,7 +46,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
@@ -110,10 +109,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>{" "}
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything ready to go! ✈"
+          : `💼 You have ${numItems} items on your list, and you already packed 
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
